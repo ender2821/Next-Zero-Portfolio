4743171cd03d3355d6aa30e28958a09fc188841e
ChadDixon23 <142241347+ChadDixon23@users.noreply.github.com>
2024-07-16 22:31:04 -0600

    style navigation desktop

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -35,7 +35,6 @@ export default async function Nav() {
   const revalidate = 60;
   const costumeConstructiodData = (await client.fetch(
     costumeConstructionQuery,
-
     {
       // @ts-ignore
       next: { revalidate: revalidate },
@@ -56,49 +55,68 @@ export default async function Nav() {
   })) as NavItem[];
 
   return (
-    <nav style={{ display: "flex", justifyContent: "center" }}>
-      <ul style={{ display: "flex", listStyle: "none" }}>
-        <li style={{ margin: "0 10px" }}>
-          <Link href="/">Home</Link>
+    <nav className="w-full absolute left-0 top-0  bg-black/60">
+      <ul className="flex">
+        <li>
+          <Link href="/" className="pt-4 pr-8 pb-4 pl-8 block">
+            Home
+          </Link>
         </li>
-        <li style={{ margin: "0 10px" }}>
-          <a href="#">Costume Construction</a>
-          <ul>
+        <li className="[&>ul]:hover:opacity-100 [&>ul]:hover:left-0 relative">
+          <p tabIndex={0} className="peer pt-4 pr-8 pb-4 pl-8 block mb-0">
+            Costume Construction
+          </p>
+          <ul className="peer-focus:opacity-100 peer-focus:left-0 focus-within:opacity-100 focus-within:left-0 opacity-0 absolute top-full -left-[9999px] backdrop-blur-xl bg-black/60">
             {costumeConstructiodData.map((item: NavItem, i: number) => (
-              <li key={item?.name + i} style={{ margin: "0 10px" }}>
-                <Link href={`/costumeConstruction/${item?.slug?.current}`}>
+              <li key={item?.name + i} className="[&>a]:last:pb-6">
+                <Link
+                  href={`/costumeConstruction/${item?.slug?.current}`}
+                  className="pt-2 pr-8 pb-2 pl-8 block"
+                >
                   {item?.name}
                 </Link>
               </li>
             ))}
           </ul>
         </li>
-        <li style={{ margin: "0 10px" }}>
-          <a href="#">Undergrad Course Work</a>
-          <ul>
+        <li className="[&>ul]:hover:opacity-100 [&>ul]:hover:left-0 relative">
+          <p tabIndex={0} className="peer pt-4 pr-8 pb-4 pl-8 block mb-0">
+            Undergrad Course Work
+          </p>
+          <ul className="peer-focus:opacity-100 peer-focus:left-0 focus-within:opacity-100 focus-within:left-0 opacity-0 absolute top-full -left-[9999px] backdrop-blur-xl bg-black/60">
             {underGradCourseWorkData.map((item: NavItem, i: number) => (
-              <li key={item?.name + i} style={{ margin: "0 10px" }}>
-                <Link href={`/undergradCourseWork/${item?.slug?.current}`}>
+              <li key={item?.name + i} className="[&>a]:last:pb-6">
+                <Link
+                  href={`/undergradCourseWork/${item?.slug?.current}`}
+                  className="pt-2 pr-8 pb-2 pl-8 block"
+                >
                   {item?.name}
                 </Link>
               </li>
             ))}
           </ul>
         </li>
-        <li style={{ margin: "0 10px" }}>
-          <a href="#">Related Work</a>
-          <ul>
+        <li className="[&>ul]:hover:opacity-100 [&>ul]:hover:left-0 relative">
+          <p tabIndex={0} className="peer pt-4 pr-8 pb-4 pl-8 block mb-0">
+            Related Work
+          </p>
+          <ul className="peer-focus:opacity-100 peer-focus:left-0 focus-within:opacity-100 focus-within:left-0 opacity-0 absolute top-full -left-[9999px] backdrop-blur-xl bg-black/60">
             {relatedWorkData.map((item: NavItem, i: number) => (
-              <li key={item?.name + i} style={{ margin: "0 10px" }}>
-                <Link href={`/relatedWork/${item?.slug?.current}`}>
+              <li key={item?.name + i} className="[&>a]:last:pb-6">
+                <Link
+                  href={`/relatedWork/${item?.slug?.current}`}
+                  className="pt-2 pr-8 pb-2 pl-8 block"
+                >
                   {item?.name}
                 </Link>
               </li>
             ))}
           </ul>
         </li>
-        <li style={{ margin: "0 10px" }}>
-          <Link href="/contact">Contact</Link>
+        <li>
+          <Link href="/contact" className="pt-4 pr-8 pb-4 pl-8 block">
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
